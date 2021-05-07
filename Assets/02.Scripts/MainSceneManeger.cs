26f45df7a963e9d7ac9bde4e9045d1e815fa362d
Hoon <totohoon01@naver.com>
2021-05-07 11:25:52 +0900

    main scene logic

--- Assets/02.Scripts/MainSceneManeger.cs
+++ Assets/02.Scripts/MainSceneManeger.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainSceneManeger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_InputField userNameInput;
+    public TMP_InputField userPWInput;
+    public TMP_Text aletText;
 
-    // Update is called once per frame
-    void Update()
+    //유저 정보를 저장할 변수 -> 싱글톤으로 구성해야함 별도의 오브젝트 및 스크립트에.
+    private static string userName;
+    private static string userPW;
+
+    public void OnStartButtonClick()
     {
-        
+        if (userNameInput.text != null && userPWInput.text != null)
+        {
+            userName = userNameInput.text;
+            userPW = userPWInput.text;
+            SceneManager.LoadScene("02.PlayScene");
+        }
+        else
+        {
+            aletText.GetComponent<TMP_Text>().enabled = true;
+            Invoke("ToggleAlret", 2.0f);
+        }
+    }
+    void ToggleAlret()
+    {
+        aletText.GetComponent<TMP_Text>().enabled = false;
     }
 }
