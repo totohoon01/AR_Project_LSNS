8b8e909ab191fe4edfdfd4dee2ca87bed42d6dde
Hoon <totohoon01@naver.com>
2021-05-10 09:47:45 +0900

    에러

--- Assets/02.Scripts/MainSceneManeger.cs
+++ Assets/02.Scripts/MainSceneManeger.cs
@@ -33,7 +33,7 @@ public class MainSceneManeger : MonoBehaviour
             //데이터 베이스에 쓰기
             string userIdentifier = GameManager.instance.userName + GameManager.instance.userPW;
             DatabaseReference mRef = FirebaseDatabase.DefaultInstance.RootReference; //루트
-            mRef.Child("users").SetValueAsync(userIdentifier); //유저 등록
+            mRef.Child("users").Child(userIdentifier).SetValueAsync(""); //유저 등록
             SceneManager.LoadScene("02.PlayScene");
         }
         else
