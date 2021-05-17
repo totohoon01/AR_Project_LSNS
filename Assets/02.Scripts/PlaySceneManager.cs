b3fabc8408e811fb6e6d5d9402e6c7d30c129f25
Hoon <totohoon01@naver.com>
2021-05-17 09:56:45 +0900

    디테일(창 투명도, 글자색, 최대 로딩수)

--- Assets/02.Scripts/PlaySceneManager.cs
+++ Assets/02.Scripts/PlaySceneManager.cs
@@ -83,6 +83,7 @@ public class PlaySceneManager : MonoBehaviour
 
     void DisplayPosts()
     {
+        int count = 0;
         print("Show");
         //읽어온 데이터 중에서, 일정 거리 이내에 있는 데이터는 출력함.
         foreach (var data in snapshot.Children)
@@ -91,20 +92,17 @@ public class PlaySceneManager : MonoBehaviour
             //놀랍게도 유령 키에 접근하려 한다.
             if (postData["userPos"] != null)
             {
-                string[] postPos = postData["userPos"].ToString().Split(',');
-                float postN = 0;
-                float postE = 0;
-                float.TryParse(postPos[0], out postN);
-                float.TryParse(postPos[0], out postE);
-                float longitude = GameManager.instance.logitude - postN;
-                float latitude = GameManager.instance.latitude - postE;
+                float[] userPos = ParsingUserPos(postData);
+                float longitude = userPos[0];
+                float latitude = userPos[1];
+
+                // 거리에 따라서 표시할지 결정.
                 // if (Math.Abs(longitude) <= 0.001 && Math.Abs(latitude) <= 0.001)
                 {
                     Vector3 genPos = new Vector3(Random.Range(-2.5f, 2.5f), Random.Range(0.0f, 1.0f), Random.Range(-2.5f, 2.5f));
                     print("Create");
 
-                    //프리팹 생성
-                    // int index = Random.Range(0, 5);
+                    //프리팹 생성 - 사용자 방향을 바라보게
                     GameObject obj = Instantiate(postPrefab, Camera.main.transform.position + genPos, Quaternion.identity);
                     Vector3 rot = new Vector3(Camera.main.transform.position.x, obj.transform.position.y, Camera.main.transform.position.z);
                     obj.transform.LookAt(rot);
@@ -114,13 +112,36 @@ public class PlaySceneManager : MonoBehaviour
                     obj.GetComponent<PrefabGenerator>().authorName = postData["userName"].ToString();
                     obj.GetComponent<PrefabGenerator>().createTime = postData["createTime"].ToString();
 
-                    //프리팹 위아래 텍스트
+                    //프리팹 표시(작성자, 시간, 내용)
                     obj.GetComponent<PrefabGenerator>().authorNameText.text = obj.GetComponent<PrefabGenerator>().authorName;
                     obj.GetComponent<PrefabGenerator>().creteTimeText.text = obj.GetComponent<PrefabGenerator>().createTime;
                     obj.GetComponent<PrefabGenerator>().message.text = postData["message"].ToString();
+
+                    if (count % 2 == 0)
+                    {
+                        Color color;
+                        ColorUtility.TryParseHtmlString("#292727", out color);
+                        obj.GetComponent<PrefabGenerator>().authorNameText.color = color;
+                        obj.GetComponent<PrefabGenerator>().creteTimeText.color = color;
+                        obj.GetComponent<PrefabGenerator>().message.color = color;
+                    }
                 }
             }
+            //10개만 불러옴
+            count++;
+            if (count > 10) break;
         }
+    }
+    float[] ParsingUserPos(IDictionary postData)
+    {
+        string[] postPos = postData["userPos"].ToString().Split(',');
+        float postN = 0;
+        float postE = 0;
+        float.TryParse(postPos[0], out postN);
+        float.TryParse(postPos[1], out postE);
+        float longitude = GameManager.instance.logitude - postN;
+        float latitude = GameManager.instance.latitude - postE;
+        return new float[] { longitude, latitude };
     }
     #endregion
 }
--- README.md
+++ README.md
@@ -1,61 +1,13 @@
 # 유니티 AR 프로젝트
+
+### <b>증강현실 기반 로컬 소셜 네트워크</b>
+
 <hr>
 
-### 😎 주제 
-- <b>증강현실 기반 로컬 소셜 네트워크</b>
-
-### 🧱 서비스 구성요소
-- UI
-    1. 시작화면
-        - 따로 회원가입 x, 인풋필드에 넣은 이름, 패스워드 사용
-        ![시작화면](./myResources/startup.PNG)
-    
-    2. 메인화면
-        - 가로 뷰에 후면 카메라 사용.
-        - 기존 포스트 터치 -> 3
-        - 새 포스트 터치 -> 4
-        ![메인화면](./myResources/view.PNG)
-   
-    3. 기존 포스트 로딩
-        - 작성자 이름, 작성 시간 표시
-        - ~~수정버튼~~
-        - 숨기기 버튼 : 해당 포스터를 표시안함.
-        - 삭제 버튼 : 사용자 이름과 암호 비교 맞으면 삭제 가능
-        ![기존 포스트](./myResources/oldPost.PNG)
-
-    4. 새로운 포스트 작성
-        - 포스트 : 콘텐츠 내용, 현재 위치에 포스트 데이터 생성 -> 2
-        - 취소 : -> 2
-        ![새로운 포스트](./myResources/newPost.PNG)
-
-### ❓ Check How to
-- Cloud Anchor
-
-## ✔ 진행 사항(To do) 
+### Today's To do! 😎
 - [ ] 디자인
+- [x] 메시지 창 색 투명도 조절
+- [x] 글자색 홀짝구분(약간 차이나게)
+- [x] 데이터 정해진 갯수안에서 랜덤하게 픽(ex 10개 다차면 그만찾아)
 
-### MainScene
-
-### PlayScene
-- [x] GPS 값 받아서 주변에 기존 포스트 표시
-
-### OldPostScene
-- [x] DB에서 내용 불러오기(작성자, 작성시간-간단히-, 내용)
-- [x] 'Delete' if(curUser == authorUser) this.delete()
-~~- [ ] 'Modifiy' if(curUser == authorUser) this.update(content, time)~~
-~~- [ ] 'Hide' curUser.hide(this)~~
-
-### NewPostScene
-
-### Else
-- [ ] Extras(ripple🐳 / Like👍)
-- [ ] Cloud Anchor
-
-### Today's To do!
-- [x] 프리팹 에셋 적용
-- [x] 프리팹에 작성자 이름, 작성일시 표시, 가능하면 내용일부도?
-- [x] 카메라 위치 초기화 안됨
-- [x] 상자가 카메라쪽 바라보게.
-- [x] 한글 및 영어 폰트
-- [ ] 코드 정리
-- [ ] 데이터 정해진 갯수안에서 랜덤하게 픽(ex 10개 다차면 그만찾아)
+292727
